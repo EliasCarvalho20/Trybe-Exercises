93af4cc32d3c06053e5302de3698031902e21c75
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-07-16 23:54:06 -0300

    Added exercise 02

---
+++ exercises_4.4/exercise-02.js
@@ -0,0 +1,17 @@
+function verificaPalindrome(word) {
+  
+  reversed = word.split("").reverse().join("");
+
+  for (i=0; i<word.length; i++) {
+
+    if (word[i].localeCompare(reversed[i])) {
+      return false;
+    }
+
+  }
+
+  return true;
+
+}
+
+console.log(verificaPalindrome("arara"));
