eb1ad917c1993a03d3e9baca57c7bce9359558c5
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-07-23 22:07:38 -0300

    Added exercise 2

--- exercises_5.3/1.1/main.js
+++ exercises_5.3/1.1/main.js
@@ -2,14 +2,10 @@ let button = document.querySelector('button');
 
 button.addEventListener('click', () => {
   let inputElements = document.querySelectorAll("input");
-
-  let tagDiv = document.createElement('div');
   let tagP = document.createElement('p');
-  tagDiv.className = 'main-content';
 
   tagP.textContent = `${inputElements[0].value} ${inputElements[1].value} 
   ${inputElements[2].value}`;
 
-  tagDiv.appendChild(tagP);
-  document.body.appendChild(tagDiv);
+  document.querySelector('#story').appendChild(tagP);
 });
