98a7cac5b1fa75b4428dc0ed885c6693a53dd03c
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-08-28 20:33:11 -0300

    Added exercises 9.2

---
+++ exercises_9.2/exercise01.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+};
+
+const fetchJoke = () => {
+  fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => console.log(data.joke))
+};
+
+window.onload = () => fetchJoke();
