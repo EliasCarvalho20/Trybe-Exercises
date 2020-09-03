4093ef58e933b0ef7c3d444d568256a2f195190d
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-09-03 12:13:17 -0300

    Removed duplicated directory

--- exercises_8.4/exercise08.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-
-const greet = (name, greeting = 'Hi') => `${greeting} ${name}`;
-
-assert.equal(greet("John"), "Hi John");
-assert.equal(greet("John", "Good morning"), "Good morning John");
-assert.equal(greet("Isabela", "Oi"), "Oi Isabela");
